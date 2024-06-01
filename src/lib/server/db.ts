693ae2ce09f6ba41c291d83b92884681200268de
Chuo Ngiu Bing <ignatius.chuo@seamlessx.com>
2024-06-01 09:29:17 +0000

    style: prettier

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,8 @@
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle'
 import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres';
+import postgres from 'postgres'
 import * as schema from '../../db/schema'
-import { DB_HOST, DB_PORT, DB_DATABASE,DB_USER ,DB_PASSWORD} from '$env/static/private'
+import { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } from '$env/static/private'
 
 // postgres db client not using rds
 const queryClient = postgres({
@@ -11,9 +11,9 @@ const queryClient = postgres({
 	host: DB_HOST,
 	user: DB_USER,
 	password: DB_PASSWORD,
-});
+})
 
-const db = drizzle(queryClient, { schema: schema });
+const db = drizzle(queryClient, { schema: schema })
 
 const adapter = new DrizzlePostgreSQLAdapter(db, schema.sessions, schema.users)
 
