0677cab31996cc4ef42a56b7b6b190479ee4e229
Chuo Ngiu Bing <ignatius.chuo@seamlessx.com>
2024-06-01 09:33:13 +0000

    feat: use custom logger class

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,14 @@
+import { Logger, errorLogger } from '$lib/log'
 import { lucia } from '$lib/server/auth'
-import type { Handle } from '@sveltejs/kit'
+import type { Handle, HandleServerError } from '@sveltejs/kit'
+
+const authHookLogger = new Logger('auth-hook')
 
 export const handle: Handle = async ({ event, resolve }) => {
+	authHookLogger.debug('handling request')
 	const sessionId = event.cookies.get(lucia.sessionCookieName)
 	if (!sessionId) {
+		authHookLogger.debug('no session id, setting to null')
 		event.locals.user = null
 		event.locals.session = null
 		return resolve(event)
@@ -11,6 +16,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	const { session, user } = await lucia.validateSession(sessionId)
 	if (session && session.fresh) {
+		authHookLogger.debug('session is fresh')
+		authHookLogger.debug('Refreshing session', user)
 		const sessionCookie = lucia.createSessionCookie(session.id)
 		// sveltekit types deviates from the de-facto standard
 		// you can use 'as any' too
@@ -20,13 +27,26 @@ export const handle: Handle = async ({ event, resolve }) => {
 		})
 	}
 	if (!session) {
+		authHookLogger.debug('no session')
 		const sessionCookie = lucia.createBlankSessionCookie()
 		event.cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
 			...sessionCookie.attributes,
 		})
 	}
+	authHookLogger.debug('setting user and session')
 	event.locals.user = user
 	event.locals.session = session
 	return resolve(event)
+}
+
+export const handleError: HandleServerError = async ({ error, message, status }) => {
+	const errorId = crypto.randomUUID()
+
+	errorLogger.debug({ 'Error ID': errorId, error, status, message })
+
+	return {
+		message: 'Whoops! Something went wrong.',
+		errorId,
+	}
 }
--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,32 +1,45 @@
-const debug = (...args: unknown[]) => {
-	console.log(...args)
+import { dev } from '$app/environment'
+
+export class Logger {
+	private name: string
+
+	constructor(name: string) {
+		this.name = name
+	}
+
+	_prefix() {
+		return `${new Date().toUTCString()} [${this.name}]`
+	}
+
+	debug(...args: unknown[]) {
+		if (!dev) {
+			return
+		}
+		console.log(this._prefix(), ...args)
+	}
+
+	info(...args: unknown[]) {
+		console.info(this._prefix(), ...args)
+	}
+
+	warn(...args: unknown[]) {
+		console.warn(this._prefix(), ...args)
+	}
+
+	error(...args: unknown[]) {
+		console.error(this._prefix(), ...args)
+	}
+
+	critical(...args: unknown[]) {
+		console.error(this._prefix(), ...args)
+	}
+
+	alert(...args: unknown[]) {
+		console.error(this._prefix(), ...args)
+	}
 }
 
-const info = (...args: unknown[]) => {
-	console.info(...args)
-}
+export const errorLogger = new Logger('error')
+export const authLogger = new Logger('auth')
 
-const warn = (...args: unknown[]) => {
-	console.warn(...args)
-}
-
-const error = (...args: unknown[]) => {
-	console.error(...args)
-}
-
-const critical = (...args: unknown[]) => {
-	console.error(...args)
-}
-
-const alert = (...args: unknown[]) => {
-	console.error(...args)
-}
-
-export const log = {
-	debug,
-	info,
-	warn,
-	error,
-	critical,
-	alert,
-}
+export const log = new Logger('general')
