95606212cd6b1bd74bd6096cb50db57cea5bbaa0
Chuo Ngiu Bing <ignatius.chuo@seamlessx.com>
2024-07-02 08:16:07 +0000

    feat: add schema for remaining tables

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -7,6 +7,10 @@ import {
 	point,
 	unique,
 	type AnyPgColumn,
+	integer,
+	text,
+	jsonb,
+	smallint,
 } from 'drizzle-orm/pg-core'
 import { ulid } from 'ulid'
 
@@ -100,9 +104,15 @@ export const users = pgTable('users', {
 	...createdAtUpdatedAt,
 })
 
-export const userRelations = relations(users, ({ many }) => ({
+export const userRelations = relations(users, ({ many, one }) => ({
 	sessions: many(sessions),
 	oAuthAccounts: many(oAuthAccounts),
+	defaultAddress: one(addreses, {
+		fields: [users.defaultAddressId],
+		references: [addreses.id],
+	}),
+	requests: many(requests),
+	reviews: many(reviews),
 }))
 
 export const emailVerificationTokens = pgTable('email_verification_tokens', {
@@ -115,16 +125,70 @@ export const emailVerificationTokens = pgTable('email_verification_tokens', {
 
 export const serviceProviders = pgTable('service_providers', {
 	id: primaryId,
-	ownedBy: id('owned_by')
+	ownedById: id('owned_by')
 		.references(() => users.id)
 		.notNull(),
-	availability: varchar('availability', {
-		length: 16,
-		enum: ['available', 'not_available'],
-	}),
-	address: id('address_id').notNull(),
+	availableAt: timestamp('available_at'),
+	addressId: id('address_id').notNull(),
 	...createdAtUpdatedAt,
 })
+
+export const serviceProvidersRelations = relations(serviceProviders, ({ one, many }) => ({
+	owner: one(users, {
+		fields: [serviceProviders.ownedById],
+		references: [users.id],
+	}),
+	address: one(addreses, {
+		fields: [serviceProviders.addressId],
+		references: [addreses.id],
+	}),
+	serviceProvidersToServices: many(serviceProvidersToServices),
+}))
+
+export const serviceProvidersToServices = pgTable('service_providers_to_services', {
+	serviceProviderId: id('service_provider_id').references(() => serviceProviders.id),
+	serviceId: id('service_id').references(() => services.id),
+})
+
+export const serviceProvidersToServicesRelations = relations(
+	serviceProvidersToServices,
+	({ one }) => ({
+		serviceProvider: one(serviceProviders, {
+			fields: [serviceProvidersToServices.serviceProviderId],
+			references: [serviceProviders.id],
+		}),
+		service: one(services, {
+			fields: [serviceProvidersToServices.serviceId],
+			references: [services.id],
+		}),
+	}),
+)
+
+export const services = pgTable('services', {
+	id: primaryId,
+	name: varchar('name'),
+	price: integer('price'),
+	categoryId: id('category_id').references(() => categories.id),
+	details: text('details'),
+	...createdAtUpdatedAt,
+})
+
+export const servicesRelations = relations(services, ({ one, many }) => ({
+	serviceProvidersToServices: many(serviceProvidersToServices),
+	category: one(categories, {
+		fields: [services.categoryId],
+		references: [categories.id],
+	}),
+}))
+
+export const categories = pgTable('categories', {
+	id: primaryId,
+	name: varchar('name'),
+})
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+	services: many(services),
+}))
 
 export const sessions = pgTable('sessions', {
 	id: varchar('id', {
@@ -138,7 +202,7 @@ export const sessions = pgTable('sessions', {
 	expiresAt: timestamp('expires_at').notNull(),
 })
 
-export const sessionRelations = relations(sessions, ({ one }) => ({
+export const sessionsRelations = relations(sessions, ({ one }) => ({
 	user: one(users, {
 		fields: [sessions.userId],
 		references: [users.id],
@@ -169,9 +233,160 @@ export const oAuthAccounts = pgTable(
 	},
 )
 
-export const oAuthAccountRelations = relations(oAuthAccounts, ({ one }) => ({
+export const oAuthAccountsRelations = relations(oAuthAccounts, ({ one }) => ({
 	user: one(users, {
 		fields: [oAuthAccounts.userId],
 		references: [users.id],
 	}),
 }))
+
+export const requests = pgTable('requests', {
+	id: primaryId,
+	requestedById: id('requested_by_id')
+		.references(() => users.id)
+		.notNull(),
+	addressId: id('address_id').references(() => addreses.id),
+	serviceProviderId: id('service_provider_id').references(() => serviceProviders.id),
+	serviceId: id('service_id')
+		.references(() => services.id)
+		.notNull(),
+	details: text('details'),
+	price: integer('price').notNull(),
+	meta: jsonb('meta'),
+	completedAt: timestamp('completed_at'),
+	paidAt: timestamp('paid_at'),
+	deletedAt: timestamp('deleted_at'),
+	...createdAtUpdatedAt,
+})
+
+export const requestsRelations = relations(requests, ({ one, many }) => ({
+	requestedBy: one(users, {
+		fields: [requests.requestedById],
+		references: [users.id],
+	}),
+	address: one(addreses, {
+		fields: [requests.addressId],
+		references: [addreses.id],
+	}),
+	serviceProvider: one(serviceProviders, {
+		fields: [requests.serviceProviderId],
+		references: [serviceProviders.id],
+	}),
+	service: one(services, {
+		fields: [requests.serviceId],
+		references: [services.id],
+	}),
+	requestImages: many(requestImages),
+	reviews: one(reviews),
+	tagRatings: many(tagRatings),
+}))
+
+export const requestImages = pgTable('request_images', {
+	id: primaryId,
+	requestId: id('request_id')
+		.references(() => requests.id)
+		.notNull(),
+	fileId: id('file_id')
+		.references(() => files.id)
+		.notNull(),
+})
+
+export const requestImagesRelations = relations(requestImages, ({ one }) => ({
+	file: one(files, {
+		fields: [requestImages.fileId],
+		references: [files.id],
+	}),
+	request: one(requests, {
+		fields: [requestImages.requestId],
+		references: [requests.id],
+	}),
+}))
+
+export const reviews = pgTable('reviews', {
+	id: primaryId,
+	requestId: id('request_id')
+		.references(() => requests.id)
+		.notNull(),
+	serviceId: id('service_id')
+		.references(() => services.id)
+		.notNull(),
+	rating: smallint('rating').notNull(),
+	comment: text('comment'),
+	...createdAtUpdatedAt,
+})
+
+export const reviewImages = pgTable('review_images', {
+	id: primaryId,
+	reviewId: id('review_id')
+		.references(() => reviews.id)
+		.notNull(),
+	fileId: id('file_id')
+		.references(() => files.id)
+		.notNull(),
+})
+
+export const reviewImagesRelations = relations(reviewImages, ({ one }) => ({
+	file: one(files, {
+		fields: [reviewImages.fileId],
+		references: [files.id],
+	}),
+	review: one(reviews, {
+		fields: [reviewImages.reviewId],
+		references: [reviews.id],
+	}),
+}))
+
+export const tags = pgTable('tags', {
+	id: primaryId,
+	name: varchar('name').notNull(),
+	serviceId: id('service_id')
+		.references(() => services.id)
+		.notNull(),
+	...createdAtUpdatedAt,
+})
+
+export const tagRatings = pgTable('tag_ratings', {
+	id: primaryId,
+	tagId: id('tag_id')
+		.references(() => tags.id)
+		.notNull(),
+	requestId: id('request_id')
+		.references(() => requests.id)
+		.notNull(),
+	...createdAtUpdatedAt,
+})
+
+export const files = pgTable('files', {
+	id: primaryId,
+	key: varchar('key').notNull(),
+	location: varchar('location', {
+		enum: ['s3'],
+	}).notNull(),
+})
+
+export const documents = pgTable('documents', {
+	id: primaryId,
+	fileId: id('file_id')
+		.references(() => files.id)
+		.notNull(),
+	serviceProviderId: id('service_provider_id')
+		.references(() => serviceProviders.id)
+		.notNull(),
+	type: varchar('type', {
+		enum: ['id', 'insurance', 'license'],
+	}).notNull(),
+	...createdAtUpdatedAt,
+})
+
+export const schedules = pgTable('schedules', {
+	id: primaryId,
+	label: varchar('label'),
+	requestId: id('request_id').references(() => requests.id),
+	serviceProviderId: id('service_provider_id')
+		.references(() => serviceProviders.id)
+		.notNull(),
+	startedAt: timestamp('start_at').notNull(),
+	endedAt: timestamp('end_at').notNull(),
+	deletedAt: timestamp('deleted_at'),
+	...createdAtUpdatedAt,
+})
