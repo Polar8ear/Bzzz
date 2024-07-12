74b56ef317aba4e1fde633431c0200ac75f3c94a
Chuo Ngiu Bing <ignatius.chuo@seamlessx.com>
2024-07-12 06:29:20 +0000

    feat: add test for 200 homepage

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,24 @@
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
+test('home page is running', async ({ page }) => {
+	const res = await page.goto('/')
+	expect(res?.status()).toBe(200)
+})
+test('home page links are running', async ({ page }) => {
 	await page.goto('/')
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
+
+	const links = await page
+		.$$eval('a', (anchors) => anchors.map((anchor) => anchor.href))
+		.then((hrefs) =>
+			hrefs.filter(
+				(href) =>
+					(!href.includes('#') && href.startsWith('http://')) ||
+					href.startsWith('https://') ||
+					href.startsWith('/'),
+			),
+		)
+	for (const link of links) {
+		const response = await page.goto(link)
+		expect(response?.status(), `Failed to get 200 status at ${link}`).toBe(200)
+	}
 })
